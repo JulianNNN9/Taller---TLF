5fc7da1b41a1df27bc6842e464fbe0f7f162e54d
JulianNNN9 <111833091+JulianNNN9@users.noreply.github.com>
2024-02-23 13:24:29 -0500

    Varios nuevos métodos añadidos

--- Actividad - TLF/Main.py
+++ Actividad - TLF/Main.py
@@ -16,3 +16,52 @@ def interseccion(conjunto1, conjunto2):
         if elemento in conjunto2: # Si el elemento se encuentra en el conjunto 2 lo agrega al resultado
             resultado.append(elemento)
     return resultado # Retorna e resultado
+
+def diferencia(conjunto1, conjunto2):
+    # Diferencia de conjuntos (conjunto1 - conjunto2)
+    resultado = [] # Inicializamos el restultado vacío
+    for elemento in conjunto1: # Recorre cada elemento del conjunto 1
+        if elemento not in conjunto2: # Si el elemento no se encuentra en el conjunto 2 se agrega al resultado
+            resultado.append(elemento)
+    return resultado # Retorna el resultado
+
+def complemento(universo, conjunto):
+    # Complemento de un conjunto en relación con un universo dado
+    resultado = [] # Inicializamos el restultado vacío
+    for elemento in universo: # Se recorre cada elemento del conjunto universal
+        if elemento not in conjunto: # Si el elemento no se encuentra en el conjunto se agrega al resultado
+            resultado.append(elemento)
+    return resultado # Retorna el resultado
+
+def combinacion(conjunto1, conjunto2):
+    # Combinación de conjuntos
+    resultado = [] # Inicializamos el restultado vacío
+    for elemento1 in conjunto1: # Recorre cada elemento del conjunto 1
+        for elemento2 in conjunto2: # Por cada elemento del conjunto 1, se le combina con cada elemento del conjunto 2
+            resultado.append((elemento1, elemento2))
+    return resultado # Retorna el resultado
+
+# Cardinalidad
+def calcular_cardinalidad(conjunto):
+
+    cardinalidad = 0 # Inicializar un contador
+    for _ in conjunto: # Iterar sobre los elementos del conjunto y aumentar el contador
+        cardinalidad += 1
+    
+    return cardinalidad # Retornar la cardinalidad
+
+# Subconjunto
+def es_subconjunto(conjunto1, conjunto2):
+    # Verificar si conjunto1 es subconjunto de conjunto2
+    for elemento in conjunto1: # Recorre cada elemento del conjunto 1
+        if elemento not in conjunto2: # Si se encuentra un elemento del conjunto 1 que no se encuentra en el conjunto 2, retorna False
+            return False
+    return True # Y si encuentra todos los elementos en el conjunto 2, retorna True
+
+# Conjuntos disjuntos
+def son_disjuntos(conjunto1, conjunto2):
+    # Verificar si conjunto1 y conjunto2 son conjuntos disjuntos
+    for elemento in conjunto1: # Recorre cada elemento del conjunto 1
+        if elemento in conjunto2: # Si encuentra un elemento del conjunto 1 en el conjunto 2, retorna False
+            return False
+    return True # Y si no encuentra ningún elemento del conjunto 1 en el conjunto 2 retorna True
